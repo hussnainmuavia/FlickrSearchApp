af24b42d30c3fc2fb335832612b1e3ceb17366b5
hussnain_muavia <hussnain.muavia@gmail.com>
2023-11-30 02:54:28 +0100

    updated destination

--- app/src/main/java/com/example/flickersearchapp/ui/navigation/Destination.kt
+++ app/src/main/java/com/example/flickersearchapp/ui/navigation/Destination.kt
@@ -2,6 +2,5 @@ package com.example.flickersearchapp.ui.navigation
 
 
 sealed class Destination(val route: String) {
-    object Splash : Destination(route = "splashScreen")
     object HomeScreen : Destination(route = "homeScreen")
 }
